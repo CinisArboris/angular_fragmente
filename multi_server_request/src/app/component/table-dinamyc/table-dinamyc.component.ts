1b4213e4c05da70461c9cf23cf12c4b2369fbb5f
Eyver Emilio Vargas Medina <eyver.evm@gmail.com>
2023-04-23 02:47:24 -0400

    update : data table din

--- multi_server_request/src/app/component/table-dinamyc/table-dinamyc.component.ts
+++ multi_server_request/src/app/component/table-dinamyc/table-dinamyc.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-table-dinamyc',
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./table-dinamyc.component.css']
 })
 export class TableDinamycComponent implements OnInit {
+  @Input() rowData: any[] = [];
+  @Input() columnData: any[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
