013120a8e568a819fde77acab70344ef7aea13c5
CinisArboris <eyver.evm@gmail.com>
2023-04-22 20:15:00 -0400

    update : interface istar

--- multi_server_request/src/app/interface/istar.ts
+++ multi_server_request/src/app/interface/istar.ts
@@ -1,2 +1,10 @@
 export interface IStar {
+    name: string;
+    constellation: string;
+    right_ascension: string;
+    declination: string;
+    apparent_magnitude: string;
+    absolute_magnitude: string;
+    distance_light_year: string;
+    spectral_class: string;
 }
