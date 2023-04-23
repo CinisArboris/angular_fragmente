31fbbe9dae1620ede1e92899ef69f88a44909ffc
Eyver Emilio Vargas Medina <eyver.evm@gmail.com>
2023-04-23 17:44:43 -0400

    update : capture and validate empty data or errors

--- multi_server_request/src/app/main/index-star/index-star.component.ts
+++ multi_server_request/src/app/main/index-star/index-star.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IStar } from 'src/app/interface/istar';
 import { StarService } from 'src/app/service/api_start/service-star.service';
+import { IErrorDinamyc } from 'src/app/interface/ierror-dinamyc';
 
 @Component({
   selector: 'app-index-star',
@@ -9,6 +10,7 @@ import { StarService } from 'src/app/service/api_start/service-star.service';
 })
 export class IndexStarComponent implements OnInit {
   stars: IStar[] = [];
+  errorData: IErrorDinamyc | null = null;
   columns: string[] = [
     'name',
     'constellation',
@@ -19,12 +21,32 @@ export class IndexStarComponent implements OnInit {
     'distance_light_year',
     'spectral_class',
   ];
+  nameStar: string = 'tau';
 
   constructor(private starService: StarService) {}
 
   ngOnInit(): void {
-    this.starService.getStarByName('Alpha').subscribe((data) => {
-      this.stars = data;
-    });
+    this.starService.getStarByName(this.nameStar).subscribe(
+      (data: IStar[]) => {
+        if (data && data.length > 0) {
+          this.stars = data;
+          // show response
+          console.log('Response:', this.stars);
+        } else {
+          console.log('No star data received from the API');
+          this.stars = [];
+        }
+      },
+      (error) => {
+        console.error('Error fetching star data:', error);
+        this.errorData = {
+          responseRequestStatusCode: error.status.toString(),
+          responseRequestMessageResponse: error.message.toString(),
+          responseRequestRemoteServer: error.url.toString(),
+          responseRequestTimestamp: new Date().toISOString(),
+        };
+      }
+    );
+    
   }
 }
