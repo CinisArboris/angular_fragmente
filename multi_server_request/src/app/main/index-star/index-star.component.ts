64b160c0adaeb260670a6b5a808ffd9d161d895f
Eyver Emilio Vargas Medina <eyver.evm@gmail.com>
2023-04-23 15:21:41 -0400

    update : call service servicestar

--- multi_server_request/src/app/main/index-star/index-star.component.ts
+++ multi_server_request/src/app/main/index-star/index-star.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { IStar } from 'src/app/interface/istar';
+import { StarService } from 'src/app/service/api_start/service-star.service';
 
 @Component({
   selector: 'app-index-star',
   templateUrl: './index-star.component.html',
-  styleUrls: ['./index-star.component.css']
+  styleUrls: ['./index-star.component.css'],
 })
 export class IndexStarComponent implements OnInit {
+  stars: IStar[] = [];
+  columns: string[] = [
+    'name',
+    'constellation',
+    'right_ascension',
+    'declination',
+    'apparent_magnitude',
+    'absolute_magnitude',
+    'distance_light_year',
+    'spectral_class',
+  ];
 
-  constructor() { }
+  constructor(private starService: StarService) {}
 
   ngOnInit(): void {
+    this.starService.getStarByName('Alpha').subscribe((data) => {
+      this.stars = data;
+    });
   }
-
 }
